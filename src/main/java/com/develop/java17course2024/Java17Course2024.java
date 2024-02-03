1f56be866f82f5e4a2186c62ef5474cdc87db2c2
AF <afv9988@gmail.com>
2024-02-03 12:53:09 -0600

    Byte and Char read-write

--- src/main/java/com/develop/java17course2024/Java17Course2024.java
+++ src/main/java/com/develop/java17course2024/Java17Course2024.java
@@ -1,9 +1,11 @@
 package com.develop.java17course2024;
 
-import exceptions.DAOException;
-import exceptions.EmployeeDAO;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
@@ -13,49 +15,47 @@ import java.util.logging.Logger;
 public class Java17Course2024 {
 
     public static void main(String... args) {
-        
-        try {
-            //try-catch
-            /*try {
-            System.out.println("About to open a file");
-            InputStream in = new FileInputStream("missingfile.txt");
-            System.out.println("File open");
-            int data = in.read();
-            in.close();
-            } catch (FileNotFoundException e) {
-            System.out.println(e.getClass().getName());
-            System.out.println("Quitting");
-            } catch (IOException e) {
-            System.out.println(e.getClass().getName());
-            System.out.println("Quitting");
+        //Byte Stream
+        /*byte[] b = new byte[128];
+        int bLen = b.length;
+        try (FileInputStream fis = new FileInputStream(args[0]);
+                FileOutputStream fos = new FileOutputStream(args[1])){
+            System.out.println("args[0] " + args[0]);
+            System.out.println("args[1] " + args[1]);
+            System.out.println("Bytes available: " + fis.available());
+            int count = 0; 
+            int read = 0;
+            while((read=fis.read(b)) != -1){
+                if(read < bLen) fos.write(b, 0, read);
+                else fos.write(b);
+                count += read;
             }
-            //Catching multiple Exceptions
-            String cartFile = "";
-            ShoppingCart cart = null;
-            try (InputStream is = new FileInputStream(cartFile)){
-            ObjectInputStream in = new ObjectInputStream(is);
-            cart = (ShoppingCart) in.readObject();
-            } catch (ClassNotFoundException | IOException e) {
-            System.out.println("Exception deserializing " + cartFile);
-            System.out.println(e);
-            }
-            
-            //Declaring Exceptions
-            try {
-            int data = ShoppingCart.readByteFromFile();
-            } catch (IOException e) {
-            System.out.println(e.getMessage());
-            }*/
-            
-            //Custom Exceptions
-            EmployeeDAO.findById(0);
-        } catch (DAOException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println("Wrote: " + count);
+        } catch (FileNotFoundException f) {
+            System.out.println("File not found: " + f);
+        } catch (IOException e){
+            System.out.println("IOException: " + e);
         }
-
         
-        
-        
+        //Char Stream
+        char[] c = new char[128];
+        int cLen = c.length;
+        try (FileReader fr = new FileReader(args[0]);
+                FileWriter fw = new FileWriter(args[1])){
+            int count = 0; 
+            int read = 0;
+            while((read=fr.read(c)) != -1){
+                if(read < cLen) fw.write(c, 0, read);
+                else fw.write(c);
+                count += read;
+            }
+            System.out.println("Wrote: " + count);
+        } catch (FileNotFoundException f) {
+            System.out.println("File not found: " + f);
+        } catch (IOException e){
+            System.out.println("IOException: " + e);
+        }*/
+        //BufferedStream
                 
     }
 }
